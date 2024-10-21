b51531f1a5a990b3d83baac1bc711be0a98548b0
AAchucky <33118172+AAchucky@users.noreply.github.com>
2024-10-21 11:29:25 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("modal-link").href = infoLink;
       
         document.getElementById("modal-review-link").href = `agregarReseña.html?bookId=${bookId}&titulo=${encodeURIComponent(titulo)}`;
-        document.getElementById("modal-view-reviews-link").href = `muestraReseñas.html?bookId=${bookId}&titulo=${encodeURIComponent(titulo)}`;
+        document.getElementById("modal-view-reviews-link").href = `muestraResenas.html?bookId=${bookId}&titulo=${encodeURIComponent(titulo)}`;
       
         // Obtener las reseñas del libro desde Firestore
         try {
